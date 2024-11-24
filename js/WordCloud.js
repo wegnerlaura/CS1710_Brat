e6dcc241b46984df110bede7766b4c19e3904262
sabamehrzad <53187194+sabamehrzad@users.noreply.github.com>
2024-11-24 16:56:02 -0500

    word cloud update 2

--- js/WordCloud.js
+++ js/WordCloud.js
@@ -1,71 +1,108 @@
-let words = [
-    {word: "amor", quantity: 5},
-    {word: "love", quantity: 4},
-    {word: "brat", quantity: 3}
-]
-let colors = ["#64dd43", "black", "#04530a", "#7b807c"]
+// use classes for all of the visualizations
+// Promises.all in the main file (HW 8)
 
-const width = 800;
-const height = 400;
+class WordCloud{
+    constructor(parentContainer, lyricsData){
+        this.parentContainer = parentContainer;
+        this.lyricsData = lyricsData;
 
-const svg = d3.select(".word-cloud-section .visual-placeholder")
-    .append("svg")
-    .attr("width", width)
-    .attr("height", height);
+        this.initVis()
+    }
 
-const fontScale = 20;
+    initVis() {
+        console.log("initVis");
+        let vis = this
+        vis.width = 800;
+        vis.height = 1200;
+        vis.colors = ["#64dd43", "black", "#04530a", "#7b807c"]
 
-d3.layout.cloud()
-    .size([width, height])
-    .words(
-        words.map(d => ({
-            text: d.word,
-            size: d.quantity * fontScale
-        }))
-    )
-    .padding(20)
-    .rotate(0)
-    .fontSize(d => d.size)
-    .spiral("archimedean")
-    .on("end", draw)
-    .start();
+        vis.svg = d3.select(".word-cloud-section .visual-placeholder")
+            .append("svg")
+            .attr("width", vis.width)
+            .attr("height", vis.height);
 
-function draw (words) {
-    svg.selectAll("*").remove();
+        vis.svg.selectAll("*").remove();
 
-    const container = svg.append("g")
-        .attr("transform", `translate(${width / 2},${height / 2})`);
+        let container = vis.svg.append("g")
+            .attr("transform", `translate(${vis.width / 2},${vis.height / 2})`);
 
-    container.append("rect")
-        .attr("x", width)
-        .attr("y", height)
-        .attr("width", width)
-        .attr("height", height)
-        .attr("fill", "white")
+        container.append("rect")
+            .attr("x", -vis.width / 2)
+            .attr("y", -vis.height / 2)
+            .attr("width", vis.width * 2)
+            .attr("height", vis.height)
+            .attr("fill", "white")
 
-    const getColor = (d) => {
-        const originalWord = words.find(w => w.text === d.text);
-        const colorIndex = Math.floor((originalWord.size / fontScale) - 1) % colors.length;
-        return colors[colorIndex];
-    };
+        vis.wrangleData()
+    }
 
-    container.selectAll("text")
-        .data(words)
-        .enter()
-        .append("text")
-        .style("font-size", d => d.size + "px")
-        .style("fill", d => getColor(d))
-        .attr("text-anchor", "middle")
-        .attr("transform", d => `translate(${[d.x, d.y]})rotate(${d.rotate})`)
-        .text((d) => d.text);
+    wrangleData() {
+        console.log("wrangleData")
+        let vis = this
+
+        let wordCloudWordsDict = {}
+        vis.wordCloudWordsArray = []
+
+        let lyrics = vis.lyricsData.map(song => song.lyrics)
+        lyrics.forEach(song_lyrics => {
+            let words_array = song_lyrics.split(" ")
+            words_array.forEach(word => {
+                if (word in wordCloudWordsDict) {
+                    wordCloudWordsDict[word] += 1
+                } else {
+                    wordCloudWordsDict[word] = 1;
+                }
+            })
+        })
+        console.log(wordCloudWordsDict)
+
+        // words.keys.forEach(d => console.log(d))
+        Object.entries(wordCloudWordsDict).forEach(d => {
+            vis.wordCloudWordsArray.push(
+                {
+                    word: d[0],
+                    quantity: d[1]
+                }
+            )
+        })
+        console.log(vis.wordCloudWordsArray)
 
 
-    console.log(words.map(d => ({
-        text: d.text,
-        size: d.size,
-        x: d.x,
-        y: d.y
-    })))
+        vis.updateVis()
+    }
+
+    updateVis() {
+        // console.log("updateVis")
+
+        let vis = this
+
+        let fontScale = 20;
+
+        function draw(words) {
+            vis.svg.selectAll("text")
+                .data(words)
+                .join("text")
+                .style("font-size", d => `${d.size}px`)
+                .style("fill", (d, i) => vis.colors[i % vis.colors.length])
+                .attr("text-anchor", "middle")
+                .attr("transform", d => `translate(${[d.x, d.y]})rotate(${d.rotate})`)
+                .text(d => d.text);
+        }
+        console.log(vis.wordCloudWordsArray)
+
+        // Create and configure the layout
+        d3.layout.cloud()
+            .size([vis.width, vis.height])
+            // issue here!
+            .words(vis.wordCloudWordsArray.map(d => ({
+                text: d.word,
+                size: d.quantity * fontScale  // Cap maximum size
+            })))
+            .padding(5)
+            .rotate(() => 0)
+            .fontSize(d => d.size)
+            .spiral("archimedean")
+            .on("end", draw)
+            .start();
+    }
 }
-
-console.log("words", words)
---
+++ js/main.js
@@ -0,0 +1,26 @@
+let myWordCloud,
+    myPersonalityQuiz;
+
+// load data using promises
+let promises = [
+    d3.csv("data/brat-lyrics.csv"),
+    d3.csv("data/brat.csv"),
+    d3.csv("data/brat-search.csv"),
+    d3.csv("data/brat_summer-search.csv"),
+    d3.csv("data/charli-search.csv")
+];
+
+Promise.all(promises)
+    .then(function (data) {
+        initMainPage(data)
+    })
+    .catch(function (err) {
+        console.log(err)
+    });
+
+ function initMainPage(data) {
+
+     // word cloud
+     myWordCloud = new WordCloud ('wordCloudContainer', data[0]);
+ }
+
